d5aec8537cb03f440ddb75b27ab8dc1bacbf76dc
Tariq Ganem <tariqg@post.bgu.ac.il>
2022-06-29 12:06:31 +0300

    last version

--- ML-assignment2/main.py
+++ ML-assignment2/main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from catboost import CatBoostClassifier as CXGB
 from sklearn import preprocessing as asd
-
+import shap
+import scikitplot as skplt
+from sklearn.model_selection import train_test_split
 
 from utils import to_file
 
@@ -16,6 +18,7 @@ if __name__ == '__main__':
     Y = balance_data.values[0:, 1232]
     output = pd.read_csv('input/X_test.csv')
     X_test = output.values[0:, 1:]
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.3)
 
     dtc = CXGB()
 
@@ -25,3 +28,13 @@ if __name__ == '__main__':
     X_test = max_abs_scaler.transform(X_test)
     predictions = dtc.predict(X_test)
     to_file(predictions, "y_test_submission_example")
+
+    # SHAP
+    explainer = shap.TreeExplainer(dtc)
+    shap_values = explainer.shap_values(X_test)
+    shap.summary_plot(shap_values, X_test)
+
+
+    # AUC
+    plt = skplt.metrics.plot_roc(Y_validation, predictions)
+    plt.show()
